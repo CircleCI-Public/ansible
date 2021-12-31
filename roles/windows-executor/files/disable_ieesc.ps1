b8cb0f56859f239bcd7159d26faf91a1e4c6deee
mrothstein74 <michael.rothstein@circleci.com>
2021-12-30 19:48:54 -0800

    test

--- roles/windows-executor/files/disable_ieesc.ps1
+++ roles/windows-executor/files/disable_ieesc.ps1
@@ -10,7 +10,21 @@ function Disable-IEESC {
     $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
     Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
     Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0
-    Write-Host "IE Enhanced Security Configuration (ESC) has been disabled."
+
+    $explorer = Get-Process Explorer -ErrorAction SilentlyContinue
+    if ($explorer) {
+        # try gracefully first
+        $explorer.CloseMainWindow()
+        # kill after five seconds
+        Sleep 5
+        if (!$explorer.HasExited) {
+            $explorer | Stop-Process -Force
+        }
+        Write-Host "IE Enhanced Security Configuration (ESC) has been disabled."
+    } else {
+        Write-Host "Explorer is not currently running"
+    }
+
 }
 
 Disable-IEESC
