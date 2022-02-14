4bf1c7022b9a6ae92848339dc85af82cf25d55bb
mrothstein74 <michael.rothstein@circleci.com>
2022-02-14 16:43:22 -0500

    using hard coded passwords

--- roles/windows-executor/files/disable_default_winservices.ps1
+++ roles/windows-executor/files/disable_default_winservices.ps1
@@ -49,9 +49,8 @@
     }
 
     function Disable-PasswordComplexity {
-        secedit /export /cfg c:\secpol.cfg(gc C:\secpol.cfg).replace("PasswordComplexity = 1", "PasswordComplexity = 0") | Out-File C:\secpol.cfg
-        secedit /configure /db c:\windows\security\local.sdb /cfg c:\secpol.cfg /areas SECURITYPOLICY
-        rm -force c:\secpol.cfg -confirm:$false
+        Install-Module MSOnline
+        Get-MsolUser | Set-MsolUser -StrongPasswordRequired $false
     }
 
     
