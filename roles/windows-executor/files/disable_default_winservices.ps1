d3a8e31f48810d214cb6e24ad53ae7d90208f6ab
mrothstein74 <michael.rothstein@circleci.com>
2022-02-14 13:39:55 -0500

    using hard coded passwords

--- roles/windows-executor/files/disable_default_winservices.ps1
+++ roles/windows-executor/files/disable_default_winservices.ps1
@@ -48,7 +48,9 @@
     }
 
     function Disable-PasswordComplexity {
-        secedit /export /cfg c:\secpol.cfg (Get-Content C:\secpol.cfg).replace("PasswordComplexity = 1", "PasswordComplexity = 0") | Out-File C:\secpol.cfg secedit /configure /db c:\windows\security\local.sdb /cfg c:\secpol.cfg /areas SECURITYPOLICY rm -force c:\secpol.cfg -confirm:$false
+        secedit /export /cfg c:\secpol.cfg(gc C:\secpol.cfg).replace("PasswordComplexity = 1", "PasswordComplexity = 0") | Out-File C:\secpol.cfg
+        secedit /configure /db c:\windows\security\local.sdb /cfg c:\secpol.cfg /areas SECURITYPOLICY
+        rm -force c:\secpol.cfg -confirm:$false
     }
 
     
