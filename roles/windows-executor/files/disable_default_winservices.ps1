7796abe9e391f9fab5e076ead954583af2e285ec
mrothstein74 <michael.rothstein@circleci.com>
2022-02-14 16:03:14 -0500

    using hard coded passwords

--- roles/windows-executor/files/disable_default_winservices.ps1
+++ roles/windows-executor/files/disable_default_winservices.ps1
@@ -1,3 +1,5 @@
+Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
+    
     function Disable-IEESC {
         $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
         $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
