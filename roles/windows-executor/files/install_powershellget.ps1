deb39eb5aa8ecf424680b0600bfed9caf28bee5b
mrothstein74 <michael.rothstein@circleci.com>
2022-02-24 21:09:22 -0500

    test

--- roles/windows-executor/files/install_powershellget.ps1
+++ roles/windows-executor/files/install_powershellget.ps1
@@ -1,5 +1,5 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Set-PSRepository -Name PSGallery -InstallationPolicy Trusted -ErrorAction Stop
-Set-Variable -Name 'ConfirmPreference' -Value 'None' -Scope Global
-Install-Package -Name PackageManagement -Force -Confirm:$false
-Install-Package -Name PowershellGet -Force -Verbose -Confirm:$false
+# Set-Variable -Name 'ConfirmPreference' -Value 'None' -Scope Global
+Install-Package -Name PackageManagement -Force 
+Install-Package -Name PowershellGet -Force -Verbose 
