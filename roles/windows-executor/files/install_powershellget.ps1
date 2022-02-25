08c2c3be0a331d847bc7b0ee4c7052dc7f228632
mrothstein74 <michael.rothstein@circleci.com>
2022-02-25 09:17:10 -0500

    test

--- roles/windows-executor/files/install_powershellget.ps1
+++ roles/windows-executor/files/install_powershellget.ps1
@@ -1,6 +1,7 @@
 # [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Set-ExecutionPolicy -ExecutionPolicy Unrestricted
 # Set-PSRepository -Name PSGallery -InstallationPolicy Trusted -ErrorAction Stop
+Install-PackageProvider -Name NuGet -Force
 Install-Module PowerShellGet -Force -AllowClobber -Confirm:$false
 # Set-Variable -Name 'ConfirmPreference' -Value 'None' -Scope Global
 #Install-Package -Name PackageManagement -Force 
