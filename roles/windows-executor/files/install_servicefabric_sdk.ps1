b728da0fb9d077943c2cc7d2f9b7a32dc8806a8f
mrothstein74 <michael.rothstein@circleci.com>
2022-09-08 20:47:26 -0400

    windows: removed hyper-v and docker desktop

--- roles/windows-executor/files/install_servicefabric_sdk.ps1
+++ roles/windows-executor/files/install_servicefabric_sdk.ps1
@@ -1,9 +1,3 @@
 Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
-
-param 
-( 
-        [Parameter(Mandatory=$True,Position=1)]
-        [string]$servicefabricsdk_version
-)
-
+$servicefabricsdk_version = $args[1]
 choco install service-fabric-sdk --version $servicefabricsdk_version -y
