2a0755e006a385abac024d41328f139bad2b0759
Jeff Chen <jeff.chen@circleci.com>
2022-11-11 11:28:27 -0800

    refactor: updates some tests with better checks

--- tests/test_base_role.py
+++ tests/test_base_role.py
@@ -3,8 +3,7 @@ import subprocess
 import os
 
 def check_repositories(repo):
-  directory = "/etc/apt/sources.list.d"
-  dirList = os.listdir(directory)
+  dirList = os.listdir("/etc/apt/sources.list.d")
   if any(repo in substring for substring in dirList):
     return 0
   else:
@@ -17,31 +16,27 @@ def test_repositories(input, expected):
 class TestBaseSettings:
   def test_timezone(self, host):
     timezone = host.file("/etc/timezone")
-    assert timezone.contains("UTC")
+    assert timezone.contains("UTC") == True
 
   def test_apt_conf(self):
     confList = ['Assume-Yes "true"', '"--force-confnew"']
-
+    missing = ''
     for str in confList:
-      if str in open("/etc/apt/apt.conf").read():
-        check = 0
-      else:
-        check = 1
-      assert check == 0
+      if str not in open("/etc/apt/apt.conf").read():
+        missing += f"{str} "
+    assert missing == '', f"Missing the following additions to apt.conf: {missing}"
 
   def test_apt_conf_fail(self):
     with pytest.raises(Exception):
       confList = ['Assume-Yes "false"', '"--force-confold"']
-
+      missing = ''
       for str in confList:
-        if str in open("/etc/apt/apt.conf").read():
-          check = 0
-        else:
-          check = 1
-        assert check == 0
+        if str not in open("/etc/apt/apt.conf").read():
+          missing += f"{str} "
+      assert missing == '', f"Intentionally missing the following additions in apt.conf: {missing}"
 
   def test_noninteractive(self):
     debianFrontend = 'Defaults    env_keep += "DEBIAN_FRONTEND"'
     cmd = subprocess.run(['sudo', 'cat', '/etc/sudoers.d/env_keep'], capture_output=True)
     cmdCheck = cmd.stdout.decode("utf-8").split('\n', 1)[0]
-    assert cmdCheck == debianFrontend
+    assert cmdCheck == debianFrontend, f"{debianFrontend} is missing from the sudoers configuration"
