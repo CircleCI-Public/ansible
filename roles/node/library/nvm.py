f5728671d9403ab3d76de428807cac787ade3b49
Jeff Chen <jeff.chen@circleci.com>
2023-04-04 18:23:33 -0700

    test

--- roles/node/library/nvm.py
+++ roles/node/library/nvm.py
@@ -25,22 +25,22 @@ def main():
             version = 'lts/*'
         elif version == 'current':
             version = 'node'
-        if subprocess.run(['sudo', 'su', '- circleci', '/bin/bash', '-i', '-c', 'source /home/circleci/.circlerc && nvm install ' + version]).returncode != 0:
+        if subprocess.run(['sudo', 'su', 'circleci', '/bin/bash', '-c', 'source /home/circleci/.circlerc && nvm install ' + version]).returncode != 0:
             module.fail_json(msg='Failed to install NodeJS ' + version + '!')
 
     if default:
         if default == 'lts':
-            if subprocess.run(['sudo', 'su', '- circleci', '/bin/bash', '-i', '-c', 'source /home/circleci/.circlerc && nvm alias default lts/*']).returncode != 0:
+            if subprocess.run(['sudo', 'su', 'circleci', '/bin/bash', '-c', 'source /home/circleci/.circlerc && nvm alias default lts/*']).returncode != 0:
                 module.fail_json(msg='Failed to set nvm alias!')
         else:
-            if subprocess.run(['sudo','su', '- circleci', '/bin/bash', '-i', '-c', 'source /home/circleci/.circlerc && nvm alias default ' + default]).returncode != 0:
+            if subprocess.run(['sudo','su', 'circleci', '/bin/bash', '-c', 'source /home/circleci/.circlerc && nvm alias default ' + default]).returncode != 0:
                 module.fail_json(msg='Failed to set nvm alias!')
-        if subprocess.run(['sudo', 'su', '- circleci', '/bin/bash', '-i', '-c', 'source /home/circleci/.circlerc && nvm use default']).returncode != 0:
+        if subprocess.run(['sudo', 'su', 'circleci', '/bin/bash', '-c', 'source /home/circleci/.circlerc && nvm use default']).returncode != 0:
             module.fail_json(msg='Failed to set default NodeJS version!')
 
     if npm:
         for package in npm:
-            if subprocess.run(['sudo', 'su', '- circleci', '/bin/bash', '-lc', 'source /home/circleci/.circlerc && npm install -g ' + package]).returncode != 0:
+            if subprocess.run(['sudo', 'su', 'circleci', '/bin/bash', '-lc', 'source /home/circleci/.circlerc && npm install -g ' + package]).returncode != 0:
                 module.fail_json(msg='Failed to install ' + package + ' globally with npm!')
 
     module.exit_json(changed=True)
