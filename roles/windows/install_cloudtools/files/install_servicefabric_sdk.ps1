d9838d752f03c8e9523ca44a65f53f040a0f0b25
ryanbourdais <ryanbourdais@outlook.com>
2023-07-26 18:43:08 -0500

    fix

--- roles/windows/install_cloudtools/files/install_servicefabric_sdk.ps1
+++ roles/windows/install_cloudtools/files/install_servicefabric_sdk.ps1
@@ -1,3 +1,3 @@
 Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
 $servicefabricsdk_version = $args[1]
-choco install service-fabric-sdk --version $servicefabricsdk_version -y
+choco install service-fabric-sdk --version $servicefabricsdk_version -y --force
