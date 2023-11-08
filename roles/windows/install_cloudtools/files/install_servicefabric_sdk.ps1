501122cdd8323647bf236925d206fff57081f642
Jeff Chen <jeff.chen@circleci.com>
2023-11-08 11:50:40 -0800

    update service fabric installation [skip ci]

--- roles/windows/install_cloudtools/files/install_servicefabric_sdk.ps1
+++ roles/windows/install_cloudtools/files/install_servicefabric_sdk.ps1
@@ -1,3 +1,3 @@
 Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
-$servicefabricsdk_version = $args[1]
+$servicefabricsdk_version = $args[0]
 choco install service-fabric-sdk --version $servicefabricsdk_version -y
